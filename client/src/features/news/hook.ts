ff356e7002fe80577090a94219f62ebc9a0980d4
BOM S. LAP <122735033+sompakorn-lap@users.noreply.github.com>
2025-02-16 18:27:49 +0700

    fix(client): error import

--- client/src/features/news/hook.ts
+++ client/src/features/news/hook.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { NewsArticle } from "./type";
+import { NewsArticle } from "./schema";
 
 export function useNews() {
   return useQuery({
