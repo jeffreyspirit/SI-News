f3e106024bc7d91a0e7ca541ee2e64b0ddac80d0
Jeffrey KH <jeffrey.khemjirath@gmail.com>
2025-03-05 09:51:27 +0700

    Update NewsList.tsx

--- client/src/features/news/NewsList.tsx
+++ client/src/features/news/NewsList.tsx
@@ -55,17 +55,7 @@ function NewsList({ news }: NewsListProps) {
   );
 
   return (
-    // <div className="container mx-auto p-4">
-    //  {/* ✅ Search and Filter Bar */}
-    //  <NewsSearchBar
-    //    initValue={{
-    //      searchQuery,
-    //      selectedCategory,
-    //      selectedStatus,
-    //      selectedYears,
-    //   }}
-    //    handleFilter={handleFilter}
-    //  />
+     <div className="container mx-auto p-4">
 
       {/* ✅ Render Filtered & Sorted News */}
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">
